42f01d07c149ccc219b751ce7cc9175c34a48b95
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2023-05-11 00:23:31 -0400

    wip: HomePage with working links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,32 @@
-import Link from "next/link";
+import { LinkProps, Button, Drawer, Link as MuiLink } from "@mui/material";
+import NextLink from 'next/link'
 
 export default function Layout({ children }: Props): JSX.Element {
   return (
     <div>
-      <nav>
-        <Link href="/">Home Page</Link>
-
-        <Link href="/signup">SignUp</Link>
-
-        <Link href="/signin">SignIn</Link>
-      </nav>
-
+      <NavDrawer />
       <main>{children}</main>
     </div>
   );
+}
+
+// Link code from: https://gist.github.com/kachar/028b6994eb6b160e2475c1bb03e33e6a
+function Link(props: LinkProps<'a'>) {
+  return <MuiLink component={NextLink} {...props} />
+}
+
+function NavDrawer() {
+  return (
+    <Drawer variant="permanent">
+      <Link href="/" variant="h4" underline="none">
+        Home Page
+      </Link>
+      <Link href="/signup" variant="h4" underline="none">
+        SignUp
+      </Link>
+      <Link href="/signin" variant="h4" underline="none">
+        SignIn
+      </Link>
+    </Drawer>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,5 @@
 import HomePage from '../features/HomePage';
 
-import { dbConnect, getCurrentUser } from "@/utils";
-
-
 export default function Home() {
   return <HomePage />
-}
-
-export async function getServerSideProps({ req, res}: any) {
-  // Redirect user if unauthenticated
-  await dbConnect();
-  const id = 123;
-  const user = await getCurrentUser({ id });
-  if (!user) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/signin",
-      },
-      props: {},
-    };
-  }
-  return {
-    props: {
-      user,
-    },
-  };
 }
